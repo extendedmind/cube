89acb75a915780ca43433634edbad43ecc5d6192
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2014-04-03 09:26:48 +0300

    Changed mongo database to more generic 'cube'

--- bin/evaluator-config.js
+++ bin/evaluator-config.js
@@ -2,7 +2,7 @@
 module.exports = {
   "mongo-host": "127.0.0.1",
   "mongo-port": 27017,
-  "mongo-database": "cube_development",
+  "mongo-database": "cube",
   "mongo-username": null,
   "mongo-password": null,
   "http-port": 1081
